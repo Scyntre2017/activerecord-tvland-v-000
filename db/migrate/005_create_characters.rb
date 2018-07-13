2d685d178ecae49d2ea5ddf0432e3413fa2f02ff
Christopher Jones <cjones999983@gmail.com>
2018-07-13 19:39:21 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,8 +2,6 @@ class CreateCharacters < ActiveRecord::Migration[4.2]
   def change
     create_table :characters do |t|
       t.string :name
-      t.integer :artist_id
-      t.integer :show_id
     end
   end
 end
